ca2f9d0584f5d9e99b0b9f5a850007ccf9ac200e
Nazmul Ahmed <grey000906@gmail.com>
2021-07-09 20:36:34 +0600

    component: add basic react-map-gl

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 function App() {
-  return <div className="App">Welcome!!!</div>;
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    />
+  );
 }
 
 export default App;

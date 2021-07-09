637e8fd3fb12b61e66978d2833f1f865e0e93f6c
Nazmul Ahmed <grey000906@gmail.com>
2021-07-09 20:45:27 +0600

    map: resize screen

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import ReactMapGL from "react-map-gl";
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
+    width: "100vw",
+    height: "100vh",
     latitude: 37.7577,
     longitude: -122.4376,
     zoom: 8,

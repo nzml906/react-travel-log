11e63f0ec5403a3c3555281bc8ee1a6de5571791
Nazmul Ahmed <grey000906@gmail.com>
2021-07-09 14:12:41 +0600

    get: find logs route /api/logs
    * get defined for route /api/logs
    * moongoose find() method

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -3,10 +3,13 @@ const LogEntry = require("../models/LogEntry");
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "Hi api/logs",
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const entries = await LogEntry.find();
+    res.json(entries);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {

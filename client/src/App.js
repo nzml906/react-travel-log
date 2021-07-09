c188a903e632c776a72646d03afc3af3209065d1
Nazmul Ahmed <grey000906@gmail.com>
2021-07-10 01:47:11 +0600

    map: fetch logentries & show
    * react-map-gl marker to show locations

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+import { listLogEntries } from "./API";
 
 function App() {
+  const [logEntries, setLogEntries] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -11,13 +13,35 @@ function App() {
     zoom: 3,
   });
 
+  const getEntries = async () => {
+    const logEntries = await listLogEntries();
+    setLogEntries(logEntries);
+    console.log(logEntries);
+  };
+
+  useEffect(() => {
+    getEntries();
+  }, []);
+
   return (
     <ReactMapGL
       {...viewport}
       mapStyle="mapbox://styles/thecjreynolds/ck117fnjy0ff61cnsclwimyay"
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    />
+    >
+      {logEntries.map((entry) => (
+        <Marker
+          key={entry._id}
+          latitude={entry.latitude}
+          longitude={entry.longitude}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <div>{entry.title}</div>
+        </Marker>
+      ))}
+    </ReactMapGL>
   );
 }
 

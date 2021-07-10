7a17bed2760a5590b6fa78ed7f12892c343d4607
Nazmul Ahmed <grey000906@gmail.com>
2021-07-10 13:39:57 +0600

    popup: form detailed
    * HTML default datepicker
    * htmlFor property

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const LogEntryForm = () => {
   return (
-    <form>
-      <label>Title</label>
-      <input />
+    <form className="entry-form">
+      <h3 className="error">ERROR!!!</h3>
+      <label htmlFor="title">Title</label>
+      <input name="title" required />
+      <label htmlFor="comments">Comments</label>
+      <textarea name="comments" rows={3}></textarea>
+      <label htmlFor="description">Description</label>
+      <textarea name="description" rows={3}></textarea>
+      <label htmlFor="image">Image</label>
+      <input name="image" />
+      <label htmlFor="visitDate">Visit Date</label>
+      <input name="visitDate" type="date" required />
+      <button>Create Entry</button>
     </form>
   );
 };

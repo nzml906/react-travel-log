83404dd8e46f969e7ad9e1dd3ea8431f0b81c2d7
Nazmul Ahmed <grey000906@gmail.com>
2021-07-10 14:09:52 +0600

    form: basic submit & view output
    * react-hook-form for easy-to-use validation.

--- client/src/LogEntryForm.js
+++ client/src/LogEntryForm.js
@@ -1,19 +1,26 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const LogEntryForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="entry-form">
+    <form onSubmit={handleSubmit(onSubmit)} className="entry-form">
       <h3 className="error">ERROR!!!</h3>
       <label htmlFor="title">Title</label>
-      <input name="title" required />
+      <input name="title" required ref={register} />
       <label htmlFor="comments">Comments</label>
-      <textarea name="comments" rows={3}></textarea>
+      <textarea name="comments" rows={3} ref={register}></textarea>
       <label htmlFor="description">Description</label>
-      <textarea name="description" rows={3}></textarea>
+      <textarea name="description" rows={3} ref={register}></textarea>
       <label htmlFor="image">Image</label>
-      <input name="image" />
+      <input name="image" ref={register} />
       <label htmlFor="visitDate">Visit Date</label>
-      <input name="visitDate" type="date" required />
+      <input name="visitDate" type="date" required ref={register} />
       <button>Create Entry</button>
     </form>
   );
